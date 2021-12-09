174d6fc51def69b9d7987d2f268debe43db4ca30
Leonhard Horstmeyer <leo.h@hotmail.de>
2021-12-10 00:45:56 +0100

    included templates and queries for the meetings table

--- app/php/queries.php
+++ app/php/queries.php
@@ -1,13 +1,25 @@
 <?php
 
     $allEventsSQL = "SELECT  
-        ev.eventId, ev.name, ev.host, ev.email, ev.from_date, ev.until_date, ev.wo, ev.beginn, ev.virtuell, ps.title, ps.subtitle , eS.description as EventStatus, eT.description as EventType
+        ev.eventId, ev.name, ev.host, ev.email, ev.from_date, ev.until_date, ev.wo, ev.beginn, ev.virtuell, ps.postId, ps.title, ps.subtitle , eS.description as EventStatus, eT.description as EventType
         FROM events ev 
         LEFT JOIN posts ps ON ev.postId=ps.postId
         LEFT JOIN gatherings gth ON ev.gatheringId=gth.gatheringId
         LEFT JOIN eventType eT ON gth.eventTypeId=eT.eventTypeId
         LEFT JOIN eventStatus eS ON ev.statusId=eS.statusId
-        ORDER BY ev.from_date ASC;";
+        ORDER BY ev.from_date DESC;";
+
+
+    $allMeetingsSQL = "SELECT mt.meetingId, mt.date, mt.location, mt.virtuell, mt.description, eS.description AS statusDescription, eS.statusId, ps.postId, ps.posttypeId, ps.title, ps.subtitle, eT.eventTypeId, eT.description AS eventTypeDescription, mnts.minutes, tn.Name AS minutesTaker
+        FROM meetings mt 
+        LEFT JOIN eventStatus eS ON mt.statusTypeId=eS.statusId
+        LEFT JOIN gatherings gth ON mt.gatheringId=gth.gatheringId
+        LEFT JOIN eventType eT ON gth.eventTypeId=eT.eventTypeId
+        LEFT JOIN posts ps ON ps.postId=mt.postId
+        LEFT JOIN `minutes` mnts ON mnts.gatheringId=gth.gatheringId
+        LEFT JOIN teilnehmer tn ON mnts.nameId=tn.id
+        ORDER BY mt.date DESC;";
+
 
     function getParticipantsOfEventSQL ($eventid) {
         return "SELECT tn.Name, tn.Affiliation
@@ -16,6 +28,15 @@
             INNER JOIN participation pc on gth.gatheringId=pc.gatheringId
             INNER JOIN teilnehmer tn on pc.nameId=tn.id
             WHERE ev.eventId=" . $eventid . ";";
+    }
+
+    function getParticipantsOfMeetingSQL ($meetingid) {
+        return "SELECT tn.Name, tn.Affiliation
+            FROM meetings mt
+            INNER JOIN gatherings gth ON gth.gatheringId=mt.gatheringId
+            INNER JOIN participation pc on gth.gatheringId=pc.gatheringId
+            INNER JOIN teilnehmer tn on pc.nameId=tn.id
+            WHERE mt.meetingId=" . $meetingid . ";";
     }
 
     function getParagraphsOfEventSQL ($eventid) {
@@ -27,13 +48,22 @@
             ORDER BY prgph.ordinal ASC;";
     }
 
-    function getImagesOfEventSQL ($eventid) {
+    function getParagraphsOfMeetingSQL ($meetingid) {
+        return "SELECT 
+            prgph.postId, prgph.text, prgph.ordinal
+            FROM meetings mt
+            INNER JOIN paragraphs prgph ON mt.postId=prgph.postId
+            WHERE mt.meetingId=" . $meetingid .  
+            " ORDER BY prgph.ordinal ASC;";
+    }
+
+    function getImagesOfPostSQL ($postid) {
         $mediatypeId = 1;
         return "SELECT md.path, md.author, md.alt
             FROM posts ps
             INNER JOIN mediaInPosts mIP ON ps.postId=mIP.postId
             INNER JOIN media md ON md.mediaId=mIP.mediaId
-            WHERE ps.postId=" . $eventid . " AND md.mediatypeId=" . $mediatypeId . ";";
+            WHERE ps.postId=" . $postid . " AND md.mediatypeId=" . $mediatypeId . ";";
     }
 
 ?>
--- app/php/templates.php
+++ app/php/templates.php
@@ -11,11 +11,11 @@ function getTextAndImages($paragraphs, $images) {
                     <img src="' . $images[$i]["path"] . '" alt="' . $images[$i]["alt"] . '" width="100%">
                     <br>Photo by: ' . $images[$i]["author"] .
                     '</div>';
-    $returnvalue = $returnvalue . $paragraphs[$i]["text"];
+    $returnvalue = $returnvalue . '<p>' . $paragraphs[$i]["text"] . '</p>';
   }
   if ($LenPara>$LenImag){
     for($i=$MinLen; $i<$LenPara;$i++) {
-      $returnvalue = $returnvalue . $paragraphs[$i]["text"]; 
+      $returnvalue = $returnvalue . '<p>' . $paragraphs[$i]["text"] . '</p>';
     }
   } elseif ($LenPara<$LenImag){
     for($i=$MinLen; $i<$LenImag;$i++) {
@@ -83,6 +83,118 @@ function getEvent($cardDOMid, $event, $paragraphs, $images, $participants) {
           </div>
         </div>';
     return $card;
+}
+
+
+function statusOfMeetingDiv($meeting) {
+  $rightness = "55px";
+  // cancelled, upcoming, ongoing, completed 
+  $bgcolors = array("#aa0000", "#dd04a0", "#e1ef06", "#a0aa04");
+  $textcolors = array("#000", "#000","#fff","#000");
+  $div = '<div style="right:' . $rightness . '; background-color:' . $bgcolors[intval($meeting["statusId"])] .
+         '; color:' . $textcolors[intval($meeting["statusId"])] . '" ' .
+              'class="bigg_box_" 
+              style="cursor:pointer;"
+              title="' . $meeting["statusDescription"] . '"><span style="cursor:default">' . 
+              strtoupper($meeting["statusDescription"][0]) . '</span></div>';
+  // $div = '<div style="right:' . $rightness . '; background-color:#dd04a0; color:#000"
+  // class="bigg_box_"
+  // style="cursor:pointer;"
+  // title="Completed"><span style="cursor:default">' . strval($meeting["statusId]) . '</span></div>';
+  return $div;
+}
+
+function virtualMeetingDiv($virtualFlag){
+  $rightness = "0px";
+  $bgcolors = array("#ccb1fa", "#440aaa");
+  $textcolors = array("#000", "#fff");
+  $status = array('Physical Meeting', 'Virtual Event');
+  $div = '<div style="right:' . $rightness . '; background-color:' . $bgcolors[intval($virtualFlag)] .
+    '; color:' . $textcolors[intval($virtualFlag)] . '" ' .
+    'class="bigg_box_" 
+    style="cursor:pointer;"
+    title="' . $status[$virtualFlag] . '"><span style="cursor:default">' . 
+    strtoupper($status[$virtualFlag][0]) . '</span></div>';
+  return $div;
+}
+
+
+function getMinutesOfMeeting($minutes, $author) {
+
+  $div = '';
+  if (!$minutes){
+    $div = '';
+  } else {
+    $div = '<details>
+              <summary><b>Minutes</b>
+              </summary>
+              <div class="minutes_">' .
+                str_replace("\r\n", "<br>", $minutes) .
+              '</div>
+              <div style="width:100%; text-align:right; color: #aa0000">Minute taker: ' . $author . '</div>
+            </details>';
+  }
+  return $div;
+}
+
+
+function getMeeting($cardDOMid, $meeting, $paragraphs, $images, $participants) {
+  
+  $title = '';
+  if ($meeting["posttypeId"]==7) {
+    // There is no post (posttypeId==empty)
+    $title = $meeting["description"];
+    $paragraphs = array();
+  } else {
+    $title = $meeting["title"];
+  }
+
+  $card = '
+  <div class="row mt-4">
+    <div class="col-sm-12 col-md-12">
+      <div class="card cardborder_ " style="margin-left: 15px; margin-right: 15px" id="' . $cardDOMid . '">
+        <div class="card-header">
+          <div style="width:75%">
+            <h4 class="padding_ center_">' . $title . '</h4>
+          </div>
+          <div style="width:25%">' .
+            statusOfMeetingDiv($meeting) .
+            virtualMeetingDiv($meeting["virtuell"]) .          
+          '</div>
+        </div>
+        <div class="card-body justify_">
+          <div class="fade_out_text_">' .
+            getTextAndImages($paragraphs, $images) .
+            '<p class="read_more_"><span class="read_more_button_">Read More</span></p>
+          </div> ' .
+          getMinutesOfMeeting($meeting["minutes"], $meeting["minutesTaker"]) .
+        '</div>
+        <div class="card-footer">
+        
+          <div class="row">
+            <div class="col-3 ">
+              <p><b>Where</b><br>' . $meeting["location"] . '</p>
+            </div>
+            <div class="col-3">
+              <p><b>When</b><br>' . strval($meeting["date"]) . '</p>
+            </div>
+            <div class="col-6">
+              <p style="position:absolute; top:0">
+                <details>
+                  <summary><b>Participants from the Project</b></summary>' .
+                    getParticipants($participants) .                
+                  '<br>
+                </details>
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>';
+
+  return $card;
 }
 
 ?>
